afb0e72572f88ab33c709fd84f8518f8aa3be172
ahmedosamax <ahmeddusama995@gmail.com>
2024-06-21 22:18:07 +0300

    six task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print numbers from 0 to 9
+ * Return: (0)
+ */
+int main(void)
+{
+	for (int num = 0; num < 10 ; num++)
+		putchar(num);
+}
