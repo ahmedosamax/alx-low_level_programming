28ebb453b0bdf3e0fa63e6e77bbc69b2caf3987e
ahmedosamax <ahmeddusama995@gmail.com>
2024-06-26 01:33:57 +0300

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet x10 - Entry point
+ * Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int y = 0;
+
+	for (; y == 10; y++)
+	{
+		int x = 'a';
+
+		for (; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
