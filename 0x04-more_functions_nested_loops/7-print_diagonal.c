e71f57da610b73a6863714e25a6eacb855fb37a7
ahmedosamax <ahmeddusama995@gmail.com>
2024-06-27 20:00:11 +0300

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,30 @@
 #include "main.h"
 /**
- * print_line - print '\'
+ * print_diagonal - print '\'
  * @n: the input Where n is the number of \ should be printed
  * Return: 0 if success
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		while (n != 0)
+		int i;
+
+		for (i = 0; i < n; i++)
 		{
+			int j;
+
+			for (j = 0; j <= i; j++)
+			{
+				if (j == 0)
+				{
+					continue;
+				}
+				_putchar(' ');
+			}
 			_putchar('\\');
-			n--;
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 		_putchar('\n');
