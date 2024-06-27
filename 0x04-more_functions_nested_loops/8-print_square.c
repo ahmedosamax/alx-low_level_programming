dc23d6b304c63b90f15cc6cff5010694a817594e
ahmedosamax <ahmeddusama995@gmail.com>
2024-06-27 20:20:40 +0300

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_square - print square in shape of '#'
- * @size: the input Where n is the number of l of the square should be printed
+ * @size: the input Where n is the number of l of the square should be printed ..
  * Return: 0 if success
  */
 void print_square(int size)
