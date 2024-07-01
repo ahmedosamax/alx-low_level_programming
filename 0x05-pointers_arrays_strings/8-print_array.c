36d38cf8cfdf70ba1b16a151a3d24880492b7c5c
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-02 01:27:53 +0300

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,11 +8,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i <= n)
+	while (i < n)
 	{
-		printf("%d",(a[i]));
-		_putchar(',');
-		_putchar(' ');
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 		i++;
 	}
 	_putchar('\n');
