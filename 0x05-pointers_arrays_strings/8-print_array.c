b386771cd3ff4808cdf156ac07a899cd20b38a90
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-02 01:13:31 +0300

    9th task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,16 @@
 #include "main.h"
+/**
+ * print_array - prints arrays of numbers
+ * @a: the number to pe printed
+ * @n: the size of array
+ */
 void print_array(int *a, int n)
 {
 	int i = 0;
+
 	while (i <= n)
 	{
-		_putchar(a[i]);
+		printf("%d",(a[i]));
 		_putchar(',');
 		_putchar(' ');
 		i++;
