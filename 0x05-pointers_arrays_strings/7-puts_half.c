9132c89d5c4f7be245b8671564b470f689398ff3
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-02 01:03:17 +0300

    fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,32 @@
 #include "main.h"
+
 /**
- *  puts_half - prints the second have of the string
+ * puts_half - prints half of a string
+ * followed by a new line
  * @str: string to be printed
  */
 void puts_half(char *str)
 {
-	int i = 0, j;
+	int len, n, i;
 
-	while (str[i] != '\0')
-		i++;
-	if (i % 2 == 0)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		j = i / 2;
-		while (str[j] != '\0')
-		{
-			_putchar(str[j]);
-			j++;
-		}
+		len++;
 	}
-	else
+
+	if (len % 2 == 0)
 	{
-		j = (i - 1) / 2;
-		while (str[j] != '\0')
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			_putchar(str[j+1]);
-			j++;
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
 		}
 	}
 	_putchar('\n');
