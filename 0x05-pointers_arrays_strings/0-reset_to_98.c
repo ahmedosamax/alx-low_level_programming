15607bc613c9385708725c7a6413f74bc421313f
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-01 20:34:32 +0300

    1st of the day

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+ * reset_to_98 - int function resest the variables to 98
+ * @n: the pointer use to reset the value to 98
+ * Return: 98
+ */
+void reset_to_98(int *n)
+{
+	return (*n = 98);
+}
