c4b1e0e80f2755a15079a6089abd9b9a933de2a6
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-04 00:33:02 +0300

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,18 @@
 #include "main.h"
+/**
+ * *leet -  function ncodes a string into 1337.
+ * @a: the array of numbers
+ * Return: the array of numbers and character
+ */
 char *leet(char *a)
 {
 	char *s = "aAeEoOtTlL";
 	char *n = "4433007711";
-	int i  , j ;
-	for (i = 0; a[i] != '\0'; i++)	
+	int i, j;
+
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		for(j = 0; j < 9; j++)
+		for (j = 0; j < 9; j++)
 		{
 			if (a[i] == s[j])
 				a[i] = n[j];
