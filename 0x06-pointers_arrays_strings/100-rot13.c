4e566e7322e4e379e16dd41520caa9ac8ce66d1e
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-04 20:06:45 +0300

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * leet - encodes a string in rot 13
+ * *rot13 - encodes a string in rot 13
  * @a: string to be encoded
  *
  * Return: the resulting string;
@@ -13,7 +13,7 @@ char *rot13(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
 			if (a[i] == input[j])
 			{
