77abd4ffcbde53c31bb32522cac21c1699bc46d2
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-04 20:04:29 +0300

    advanced

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string in rot 13
+ * @a: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *rot13(char *a)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; j < 54; j++)
+		{
+			if (a[i] == input[j])
+			{
+				a[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (a);
+}
