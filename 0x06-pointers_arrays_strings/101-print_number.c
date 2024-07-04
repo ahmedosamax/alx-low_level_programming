01ae56819f6e21db9a1b6e1df41b3d097df3ef32
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-04 20:34:49 +0300

    advanced task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - print number using _putchar
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == '0')
+		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
-
+#include<stdio.h>
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
