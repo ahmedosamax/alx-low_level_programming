99f69a6ec9c55b16fd49ba05569f6bd3e439653f
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-07 19:51:04 +0300

    1st task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * *_memset - function fills the first n bytes of array with char
+ * @s: the array of character
+ * @b: the character to be copied
+ * @n: number that the character copied
+ *
+ * Return: s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	 unsigned int i;
