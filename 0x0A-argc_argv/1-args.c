4c449446d187b984716653d54f673b7ddfbd35bc
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-13 03:36:33 +0300

    2nd task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,14 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
