65ee1e5efa89f2c1a113173b4fa4527d2ceef2d0
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-16 01:49:35 +0300

    3rd task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * str_concat - concatenate 2 strings
+ * @s1: the first array
+ * @s2: the second array
+ * Return: array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int count;
+	char *ptr;
+	int x;
+	int y;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	count = i + j;
+	ptr = (char *) malloc((count + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (x = 0; x < i ; x++)
+		ptr[x] = s1[x];
+	for (y = 0; y < j; y++)
+	{
+		ptr[x] = s2[y];
+		x++;
+	}
+	return (ptr);
+}
