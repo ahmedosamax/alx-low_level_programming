2336a76a2431a44b3977900d87e414ff10dfd0a2
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-18 17:50:14 +0300

    2nd task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * *string_nconcat - concatenate 2 strings
+ * @s1: the first array
+ * @s2: the second array
+ * @n: integer of array
+ * Return: array
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int count;
+	char *ptr;
+	unsigned int x;
+	unsigned int y;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; j < n; j++)
+		;
+	count = i + j;
+	ptr = (char *) malloc((count + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (x = 0; x < i ; x++)
+		ptr[x] = s1[x];
+	for (y = 0; y < j; y++)
+	{
+		ptr[x] = s2[y];
+		x++;
+	}
+	return (ptr);
+}
