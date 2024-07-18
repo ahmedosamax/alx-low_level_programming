792f892eaad08d5af0bf12cba70e30c8a469b201
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-18 18:40:02 +0300

    2nd task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && s2[j] != '\0'; j++)
 		;
 	count = i + j;
 	ptr = (char *) malloc((count + 1) * sizeof(char));
