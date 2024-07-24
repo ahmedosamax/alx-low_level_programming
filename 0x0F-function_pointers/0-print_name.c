fdf5c2d4941bee94438646bf21ffc200dc351111
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-24 20:38:13 +0300

    1st task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
+/**
+ * print_name - prints name given
+ * @name: name wanted to be printed
+ * @f: pointer to the function which will print the name
+ */
 #include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
