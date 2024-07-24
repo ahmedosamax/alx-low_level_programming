763435d70a131c9df7a52a857e80d870a3c74eb0
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-24 20:50:42 +0300

    2nd task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!size || array == NULL)
+	if (!size || array == NULL || action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
