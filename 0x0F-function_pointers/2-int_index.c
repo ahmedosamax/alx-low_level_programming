502276927e07ed967408754d6e223efdb32e0282
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-24 21:16:51 +0300

    3rd task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,9 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		cmp(array[i]);
 		if (cmp(array[i]) != 0)
 		{
-			return(i);
-			break;
+			return (i);
 		}
 	}
-	return(-1);
+	return (-1);
 }
