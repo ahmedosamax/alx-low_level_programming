783c2d132e961ed5327d3196d63c7aa45b545b8e
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-24 20:49:30 +0300

    2nd task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as par on array
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to the function we need to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (!size || array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
