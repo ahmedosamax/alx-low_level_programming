60b4c6646dd527f878173a5deb4db8bc6a97da34
ahmedosamax <ahmeddusama995@gmail.com>
2024-07-26 03:23:13 +0300

    3rd task

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints string given 
+ * @separator: the separator between strings
+ * @n: number of args
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *ptr;
+
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(list, char *);
+		if (!ptr)
+			ptr = "(nil)";
+		if (!separator)
+			printf("%s", ptr);
+		else if (separator && i == 0)
+			printf("%s", ptr);
+		else
+			printf("%s%s", separator, ptr);
+	}
+
+	printf("\n");
+
+	va_end(list);
+}
