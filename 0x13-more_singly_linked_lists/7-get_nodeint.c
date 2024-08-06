1d43d69c51fce6d06d2977e93ce3f9f79b286e17
ahmedosamax <ahmeddusama995@gmail.com>
2024-08-07 01:42:28 +0300

    8th task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
  * get_nodeint_at_index - get the element which index is given
  * @head: 1st element in list
  * @index: the index of element we want to get
- * Return: ptr to the node 
+ * Return: ptr to the node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
